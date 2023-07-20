5a27046f6d6129b1e0ef2b77f920d159d77d4e0a
Nadezhda Saunanen <saunanen@yandex.ru>
2023-07-20 06:08:52 +0300

    Решение задачи 47

---
+++ Example_47/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = random.NextDouble() * 100; 
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+
+// Не получается сделать через методы, выходят ошибки, никак не могу понятьЮ, как исправить. 
+// Ниже привела вариант, но он не работает.
+
+
+
+// double[,] matrix = GetMatrix(m, n);
+
+// double[,] GetMatrix(int m, int n)
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++) 
+//         {
+//             matrix[i,j] = Random.NextDouble() * 100;
+//         }
+//         return matrix;
+//     }
+// }
+
+// Random random = new Random();
+// void PrintMatrix(double[,] matrix)
+// { 
+//     for (int i = 0; i < m; i++)
+//     { 
+//         for (int j = 0; j < n; j++)
+//         {
+//              Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// GetMatrix(matrix);
+
+// PrintMatrix(matrix);
+
